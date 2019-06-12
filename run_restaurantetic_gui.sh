97a86e02c8d49c2f2715b17551e075e1c17cddd3
Jose Luis Cáceres Escudero <joseluiscacere8906@gmail.com>
2019-06-11 22:11:28 -0500

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   run_restaurantetic_gui.sh

--- run_restaurantetic_gui.sh
+++ run_restaurantetic_gui.sh
@@ -1,6 +1,6 @@
 docker run -it\
-        --name restaurantetic-gui\
-        -p 4000:4000\
-        -v $HOME/Developments/restaurantetic-gui:/app\
-        -d node:10.15.3-stretch
-
+	-w /app \
+	--name restaurantetic-gui\
+	-p 4000:4000\
+	-v $(pwd):/app\
+	-d node:10.15.3-alpine
