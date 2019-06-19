766cc097e586689398d086edc2f34ca943da0e45
Jose Luis Cáceres Escudero <joseluiscacere8906@gmail.com>
2019-06-19 13:25:49 -0500

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   src/app/login/login.component.html
    	modified:   src/app/pedido/pedido.component.ts
    	new file:   src/assets/logo-pedir-de-boca.svg

--- src/app/pedido/pedido.component.ts
+++ src/app/pedido/pedido.component.ts
@@ -1,13 +1,38 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { MqttService, Topic } from "../utils/mqtt.service";
+import { NotificationService, MessageType } from "../notification/notification.service";
+import { Subscription } from "rxjs";
+import { Message } from "paho-mqtt";
+import { PedidoServiceActions } from "./pedido.service";
+import { ItemEstados } from "./item";
 
 @Component({
   selector: "app-pedido",
   templateUrl: "./pedido.component.html",
   styleUrls: ["./pedido.component.scss"],
 })
-export class PedidoComponent implements OnInit {
+export class PedidoComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  subscriptions: Subscription;
+
+  constructor(private mqttService: MqttService, private notificationService: NotificationService) {
+    this.subscriptions = new Subscription();
+    
+    this.mqttService.subscribe(Topic.Servicio);
+    this.subscriptions.add(this.mqttService.message$.subscribe((msg: Message) => this.handleMessages(JSON.parse(msg.payloadString))));
+  }
 
   ngOnInit() { }
+
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
+  }
+
+  handleMessages(msg: any): void {
+    if (msg.method === PedidoServiceActions.ChangeItemState) {
+      if (msg.item.estado === ItemEstados.Listo) {
+        this.notificationService.showMessage(`Pedido ${msg.pedido} - item ${msg.item.producto.codigo} listo.`, MessageType.Info);
+      }
+    }
+  }
 }
